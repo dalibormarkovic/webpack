d66e88a8a8aa518fba6ac489d5f8bc9de8da47c6
Dalibor Markovic <60052021+dalibormarkovic@users.noreply.github.com>
2020-10-27 21:37:19 +0100

    Delete app.js

--- app.js
+++
@@ -1,6 +0,0 @@
-let showSecret = false
-let secretButton = document.querySelector('#secret-button')
-secretButton.addEventListener('click',toggleSecretStat)
-function toggleSecretStat(){
-    showSecret=!showSecret
-}
